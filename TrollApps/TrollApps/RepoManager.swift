8b30644f90353d67622befc2889fb2e77bb52ec4
Cleo <cleo.debeau@gmail.com>
2023-12-04 16:02:06 -0500

    Updated Code (bad commit message lol)

---
+++ TrollApps/TrollApps/RepoManager.swift
@@ -0,0 +1,18 @@
+//
+//  RepoManager.swift
+//  TrollApps
+//
+//  Created by Cleo Debeau on 2023-12-02.
+//
+
+import SwiftUI
+
+struct RepoManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RepoManager()
+}
