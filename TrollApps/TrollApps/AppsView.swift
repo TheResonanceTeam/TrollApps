2303f5cfa22823524024c6cf804b4b8323feb6e6
Cleo <cleo.debeau@gmail.com>
2023-12-09 16:12:04 -0500

    Version 2.1 Updates

---
+++ TrollApps/TrollApps/AppsView.swift
@@ -0,0 +1,18 @@
+//
+//  AppsView.swift
+//  TrollApps
+//
+//  Created by Cleo Debeau on 2023-12-08.
+//
+
+import SwiftUI
+
+struct AppsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppsView()
+}
---
+++ TrollApps/TrollApps/SourcesViewBeta.swift
@@ -0,0 +1,18 @@
+//
+//  SourcesViewBeta.swift
+//  TrollApps
+//
+//  Created by Cleo Debeau on 2023-12-07.
+//
+
+import SwiftUI
+
+struct SourcesViewBeta: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SourcesViewBeta()
+}
