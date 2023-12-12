a94620cb6f7c1cc03b58fc62b232f2eca70b8c0f
Cleo <cleo.debeau@gmail.com>
2023-12-11 19:00:35 -0500

    Pushed beta code and localization things

--- TrollApps/TrollApps/AppsView.swift
+++ TrollApps/TrollApps/AppsView.swift
@@ -9,48 +9,48 @@ import SDWebImageSwiftUI
 
 struct AppsView: View {
     @EnvironmentObject var repoManager: RepositoryManager
-    @State private var searchText = ""
+    @State var showFullVersion: Bool = false
 
-    func commonView() -> some View {
-        List(filteredApps) { app in
-            HStack {
-//                WebImage(url: URL(string: app.iconURL))
-//                    .resizable()
-//                    .frame(width: 30, height: 30)
-//                    .clipShape(RoundedRectangle(cornerRadius: 7))
-                VStack(alignment: .leading) {
-                    Text(app.name)
-                    Text(app.id)
-                        .font(.caption)
-                        .foregroundColor(.gray)
-                }
-            }
-        }
-        .environment(\.defaultMinListRowHeight, 50)
-        .listStyle(PlainListStyle())
-        .navigationTitle("Installed Apps")
-        .navigationBarTitle("", displayMode: .inline)
-    }
-
+    private let adaptiveCollums = [
+        GridItem(.adaptive(minimum: 65))
+    ]
 
     var body: some View {
-        if #available(iOS 15.0, *) {
-            VStack {
-                commonView()
+        NavigationView {
+            List {
+                ForEach(filteredApps.sorted { $0.name < $1.name }) { app in
+                    HStack(alignment: .top) {
+                        Image(uiImage: app.icon)
+                            .resizable()
+                            .frame(width: 48, height: 48)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .padding(.trailing, 8)
+                        VStack(alignment: .leading) {
+                            Text(app.name)
+                            Text(app.id)
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                            CollapsibleText(text: app.version , isExpanded: $showFullVersion, maxLines: 1)
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                        Spacer()
+                        
+                        Button("OPEN") {
+                            OpenApp(app.id)
+                        }
+                        .buttonStyle(AppStoreStyle(type: "gray", dissabled: false))
+
+                    }
+                }
             }
-            .searchable(text: $searchText)
+            .listStyle(PlainListStyle())
+            .listRowInsets(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 20))
             .environment(\.defaultMinListRowHeight, 50)
-            .navigationTitle("Installed Apps")
-            .navigationBarTitle("", displayMode: .inline)
-        } else {
-            VStack {
-                SearchBar(searchText: $searchText)
-                commonView()
-            }
-            .environment(\.defaultMinListRowHeight, 50)
-            .navigationTitle("Installed Apps")
-            .navigationBarTitle("", displayMode: .inline)
+            .listStyle(PlainListStyle())
+            .navigationTitle("INSTALLED_APPS")
         }
+        .navigationViewStyle(.stack)
     }
     
     var filteredApps: [BundledApp] {
@@ -59,3 +59,9 @@ struct AppsView: View {
         }
     }
 }
+                  
+                  
+                  //                    .background(
+                  //                        NavigationLink("", destination: AppDetailsView(appDetails: app))
+                  //                            .opacity(0)
+                  //                    )
