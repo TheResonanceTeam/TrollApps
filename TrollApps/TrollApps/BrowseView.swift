602ac6a78151097f12d48a1c004119655c7dd321
Cleo <cleo.debeau@gmail.com>
2023-12-11 19:00:35 -0500

    Pushed beta code and localization things

---
+++ TrollApps/TrollApps/BrowseView.swift
@@ -0,0 +1,18 @@
+//
+//  BrowseView.swift
+//  TrollApps
+//
+//  Created by Cleo Debeau on 2023-12-09.
+//
+
+import SwiftUI
+
+struct BrowseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrowseView()
+}
