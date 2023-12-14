e1823b104e0ce9f6adccf837eb6a203be104331d
Cleo <cleo.debeau@gmail.com>
2023-12-13 22:11:08 -0500

    Added language select and (unfinished) animations

---
+++ TrollApps/TrollApps/TestingView.swift
@@ -0,0 +1,18 @@
+//
+//  TestingView.swift
+//  TrollApps
+//
+//  Created by Cleo Debeau on 2023-12-13.
+//
+
+import SwiftUI
+
+struct TestingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestingView()
+}
---
+++ TrollApps/TrollApps/UserSettings.swift
@@ -0,0 +1,18 @@
+//
+//  UserSettings.swift
+//  TrollApps
+//
+//  Created by Cleo Debeau on 2023-12-12.
+//
+
+import SwiftUI
+
+struct UserSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserSettings()
+}
